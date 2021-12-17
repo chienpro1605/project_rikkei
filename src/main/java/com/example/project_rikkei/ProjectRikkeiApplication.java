b314b5504da4b3dbae43147b27dceba6c7b1436e
chien.ld <leducchien160597>
2021-12-17 09:03:22 +0700

    project

---
+++ src/main/java/com/example/project_rikkei/ProjectRikkeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_rikkei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRikkeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRikkeiApplication.class, args);
+    }
+
+}
