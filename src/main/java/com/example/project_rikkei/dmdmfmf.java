c506291f9fc9f6b4d9da74c788e5ac634bab45f2
chien1605 <81791500+chienpro1605@users.noreply.github.com>
2024-05-26 09:00:55 +0700

    test commit

---
+++ src/main/java/com/example/project_rikkei/dmdmfmf.java
@@ -0,0 +1,6 @@
+package com.example.project_rikkei;
+
+public class dmdmfmf {
+
+    private int a;
+}
---
+++ src/main/java/com/example/project_rikkei/reggghg.java
@@ -0,0 +1,6 @@
+package com.example.project_rikkei;
+
+public class reggghg {
+
+    private String asjdjfj;
+}
