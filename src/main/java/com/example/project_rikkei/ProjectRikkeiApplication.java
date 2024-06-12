05351a2a3e2e19bbc8fb955c9421d114b594fb45
chien1605 <81791500+chienpro1605@users.noreply.github.com>
2024-06-12 20:39:34 +0700

    gtthtwef

--- src/main/java/com/example/project_rikkei/ProjectRikkeiApplication.java
+++ src/main/java/com/example/project_rikkei/ProjectRikkeiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectRikkeiApplication {
 
     public static void main(String[] args) {
+
+        ewreggrg
         SpringApplication.run(ProjectRikkeiApplication.class, args);
     }
 
